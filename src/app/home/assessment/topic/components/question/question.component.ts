b95a9a66805890752bbdacbdc8e15bcb1351e91f
Theresa <theresa.oort@gmail.com>
2021-06-15 15:57:38 +0200

    More tests

--- src/app/home/assessment/topic/components/question/question.component.ts
+++ src/app/home/assessment/topic/components/question/question.component.ts
@@ -22,9 +22,9 @@ export class QuestionComponent implements OnInit {
 
   question: GeneralQuestion;
   questionIndex: number;
+  bool: boolean = false; 
 
   answer: GeneralAnswer;
-     
 
   private dateStart: Moment;
 
