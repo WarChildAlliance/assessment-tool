3c8f9d72f6d5fc4924bbb27337192146168aaff4
adrianmujica <adrian@reliefapplications.org>
2021-06-17 14:55:30 +0100

    style: added feedback style to radio button on select question

--- src/app/home/assessment/topic/components/question-select/question-select.component.ts
+++ src/app/home/assessment/topic/components/question-select/question-select.component.ts
@@ -4,97 +4,108 @@ import { AnswerSelect } from 'src/app/core/models/answer.model';
 import { QuestionSelect, SelectOption } from 'src/app/core/models/question.model';
 
 @Component({
-  selector: 'app-question-select',
-  templateUrl: './question-select.component.html',
-  styleUrls: ['./question-select.component.scss']
+    selector: 'app-question-select',
+    templateUrl: './question-select.component.html',
+    styleUrls: ['./question-select.component.scss']
 })
 export class QuestionSelectComponent implements OnInit {
-  @Input() answer: AnswerSelect;
-  @Output() answerChange = new EventEmitter<AnswerSelect>();
+    @Input() answer: AnswerSelect;
+    @Input() displayCorrectAnswer = false;
 
-  private recievedQuestion: QuestionSelect;
+    @Output() answerChange = new EventEmitter<AnswerSelect>();
 
-  @Input() set question(value: QuestionSelect) {
-     this.recievedQuestion = value;
+    private recievedQuestion: QuestionSelect;
 
-     if (this.recievedQuestion.multiple) {
-       this.generateMultipleSelectForm();
-     }
-  }
+    @Input() set question(value: QuestionSelect) {
+        this.recievedQuestion = value;
 
-  get question(): QuestionSelect {
-      return this.recievedQuestion;
-  }
-
-
-  valueForm = new FormControl(null);
-  multipleSelectForm: FormGroup = new FormGroup({
-    selectedOptions: new FormArray([]),
-  });
-
-  selectedOptions = [];
-
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit(): void {
-
-    if (this.question.multiple) {
-
-      this.multipleSelectForm.valueChanges.subscribe(value => {
-        this.selectedOptions = [];
-        value.selectedOptions.forEach((val, index) => {
-          if (val.selected) {
-            this.selectedOptions.push(this.question.options[index]);
-            this.answer = {
-              selected_options: this.formatSelectedOptions(this.question.options[index]),
-              question: this.question.id,
-              duration: 0,
-              valid: this.isValid()
-            };
-            this.answerChange.emit(this.answer);
-          }
-        });
-      });
-    } else {
-      this.valueForm.valueChanges.subscribe(value => {
-        if (value) {
-          this.answer = {
-            selected_options: this.formatSelectedOptions(value),
-            question: this.question.id,
-            duration: 0,
-            valid: this.isValid()
-          };
-          this.answerChange.emit(this.answer);
+        if (this.recievedQuestion.multiple) {
+            this.generateMultipleSelectForm();
         }
-      });
     }
-  }
 
-  private generateMultipleSelectForm(): void {
-    const selectedOptionsForm = this.multipleSelectForm.get('selectedOptions') as FormArray;
+    get question(): QuestionSelect {
+        return this.recievedQuestion;
+    }
 
-    this.question.options.forEach((option) => {
-      const selectOption = this.formBuilder.group({
-        selected: new FormControl(false),
-      });
-      selectedOptionsForm.push(selectOption);
+
+    valueForm = new FormControl(null);
+    multipleSelectForm: FormGroup = new FormGroup({
+        selectedOptions: new FormArray([]),
     });
-  }
 
-  private isValid(): boolean {
-    if (!this.question.multiple) {
-      return this.valueForm.value.valid;
-    }
-    const validOptionsLength = this.question.options.filter(option => option.valid).length;
-    const validSelectedOptionsLength = this.selectedOptions.filter(option => option.valid).length;
-    return validOptionsLength === validSelectedOptionsLength;
-  }
+    selectedOptions = [];
 
-  private formatSelectedOptions(value: SelectOption | SelectOption[]): number[] {
-    if (Array.isArray(value)) {
-      return value.map(option => option.id);
+    constructor(private formBuilder: FormBuilder) {
     }
 
-    return [value.id];
-  }
+    ngOnInit(): void {
+
+        console.log('QUESTION', this.question);
+
+        if (this.question.multiple) {
+
+            this.multipleSelectForm.valueChanges.subscribe(value => {
+                this.selectedOptions = [];
+                value.selectedOptions.forEach((val, index) => {
+                    if (val.selected) {
+                        this.selectedOptions.push(this.question.options[index]);
+                        this.answer = {
+                            selected_options: this.formatSelectedOptions(this.question.options[index]),
+                            question: this.question.id,
+                            duration: 0,
+                            valid: this.isValid()
+                        };
+                        this.answerChange.emit(this.answer);
+                    }
+                });
+            });
+        } else {
+            this.valueForm.valueChanges.subscribe(value => {
+                if (value) {
+                    this.answer = {
+                        selected_options: this.formatSelectedOptions(value),
+                        question: this.question.id,
+                        duration: 0,
+                        valid: this.isValid()
+                    };
+                    console.log('ANSWER', this.answer);
+                    this.answerChange.emit(this.answer);
+                }
+            });
+        }
+    }
+
+    private generateMultipleSelectForm(): void {
+        const selectedOptionsForm = this.multipleSelectForm.get('selectedOptions') as FormArray;
+
+        this.question.options.forEach((option) => {
+            const selectOption = this.formBuilder.group({
+                selected: new FormControl(false),
+            });
+            selectedOptionsForm.push(selectOption);
+        });
+    }
+
+    private isValid(): boolean {
+        if (!this.question.multiple) {
+            return this.valueForm.value.valid;
+        }
+        const validOptionsLength = this.question.options.filter(option => option.valid).length;
+        const validSelectedOptionsLength = this.selectedOptions.filter(option => option.valid).length;
+        return validOptionsLength === validSelectedOptionsLength;
+    }
+
+    private formatSelectedOptions(value: SelectOption | SelectOption[]): number[] {
+        if (Array.isArray(value)) {
+            return value.map(option => option.id);
+        }
+
+        return [value.id];
+    }
+
+    setAnswerBackground(option: SelectOption): string {
+        return this.displayCorrectAnswer && (option.id === this.answer.selected_options[0]) && !option.valid ? '#F2836B'
+            : this.displayCorrectAnswer && option.valid ? '#7EBF9A' : '';
+    }
 }
