94facd6ec6b7871256dc3ce620ecfafe04bc206f
adrianmujica <adrian@reliefapplications.org>
2021-06-17 11:15:37 +0100

    feat: implemented continue button on question component

--- src/app/home/assessment/topic/components/question/question.component.ts
+++ src/app/home/assessment/topic/components/question/question.component.ts
@@ -13,91 +13,95 @@ import { AssessmentService } from 'src/app/core/services/assessment.service';
 import { Assessment } from 'src/app/core/models/assessment.model';
 
 @Component({
-  selector: 'app-question',
-  templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+    selector: 'app-question',
+    templateUrl: './question.component.html',
+    styleUrls: ['./question.component.scss']
 })
 export class QuestionComponent implements OnInit {
-  topic: Topic;
+    topic: Topic;
 
-  question: GeneralQuestion;
-  questionIndex: number;
+    question: GeneralQuestion;
+    questionIndex: number;
 
-  answer: GeneralAnswer;
+    displayCorrectAnswer = false;
 
-  private dateStart: Moment;
+    answer: GeneralAnswer;
 
-  private assessment: Assessment;
-  firstTry: boolean;
+    private dateStart: Moment;
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private answerService: AnswerService,
-    public dialog: MatDialog,
-    private assessmentService: AssessmentService
-  ) { }
+    private assessment: Assessment;
+    firstTry: boolean;
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private answerService: AnswerService,
+        public dialog: MatDialog,
+        private assessmentService: AssessmentService
+    ) {
+    }
 
 
-  ngOnInit(): void {
-    this.dateStart = moment();
+    ngOnInit(): void {
+        this.dateStart = moment();
 
-    combineLatest([this.route.data, this.route.paramMap]).subscribe(
-      ([data, params]: [{ topic: any }, ParamMap]) => {
-        if (data && params) {
-          this.assessmentService.getAssessment(data.topic.assessment).subscribe( res => {
-            this.assessment = res;
-            this.isFirst(this.topic.id);
-          });
-          this.topic = data.topic;
-          const questionId = parseInt(params.get('question_id'), 10);
-          this.questionIndex = data.topic.questions.findIndex(q => q.id === questionId);
-          this.question = data.topic.questions[this.questionIndex];
+        combineLatest([this.route.data, this.route.paramMap]).subscribe(
+            ([data, params]: [{ topic: any }, ParamMap]) => {
+                if (data && params) {
+                    this.assessmentService.getAssessment(data.topic.assessment).subscribe(res => {
+                        this.assessment = res;
+                        this.isFirst(this.topic.id);
+                    });
+                    this.topic = data.topic;
+                    const questionId = parseInt(params.get('question_id'), 10);
+                    this.questionIndex = data.topic.questions.findIndex(q => q.id === questionId);
+                    this.question = data.topic.questions[this.questionIndex];
+                }
+            }
+        );
+
+    }
+
+    submitAnswer(): void {
+        const duration = moment.duration(moment().diff(this.dateStart));
+        if (this.answer) {
+            this.answer.duration = duration.asMilliseconds();
+            if (this.canShowFeedback()) {
+                this.displayCorrectAnswer = true;
+            } else {
+                this.submitAndGoNextPage();
+            }
+        } else {
+            this.goToNextPage();
         }
-      }
-    );
+    }
 
-  }
-
-  submitAnswer(): void{
-    const duration = moment.duration(moment().diff(this.dateStart));
-    if (this.answer) {
-      this.answer.duration = duration.asMilliseconds();
-      // if we have feedback on 1 == SHOW_ALWAYS, or on 2 == SHOW_ON_SECOND_TRY
-      if (this.topic.show_feedback === 1 || (this.topic.show_feedback === 2 && !this.firstTry)){
-        const dialogRef = this.dialog.open(FeedbackComponent, {
-          data: {answer: this.answer, solution: this.question, valid: this.answer.valid}
-        });
-        dialogRef.afterClosed().subscribe(_ => {
-          this.answerService.submitAnswer(this.answer).subscribe(res => {
+    submitAndGoNextPage(): void {
+        this.answerService.submitAnswer(this.answer).subscribe(res => {
             this.goToNextPage();
             this.answer = null;
-          });
         });
-      } else {
-        this.answerService.submitAnswer(this.answer).subscribe(res => {
-          this.goToNextPage();
-          this.answer = null;
+    }
+
+    isFirst(topicId): any {
+        return this.answerService.getCompleteStudentAnswersForTopic(topicId).subscribe(topics => {
+            this.firstTry = topics.length === 0;
         });
-      }
-    } else {
-      this.goToNextPage();
     }
-  }
 
-  isFirst(topicId): any{
-    return this.answerService.getCompleteStudentAnswersForTopic(topicId).subscribe( topics => {
-      this.firstTry = topics.length === 0;
-    });
-  }
-
-  private goToNextPage(): void {
-    if (this.questionIndex + 1 < this.topic.questions.length) {
-      const nextId = this.topic.questions[this.questionIndex + 1].id;
-      this.router.navigate(['../', nextId], { relativeTo: this.route });
-    } else {
-      this.router.navigate(['../../', 'completed'], { relativeTo: this.route });
-      // this.router.navigate(['../../../../'], { relativeTo: this.route });
+    canShowFeedback(): boolean {
+        // if we have feedback on 1 == SHOW_ALWAYS, or on 2 == SHOW_ON_SECOND_TRY
+        return this.topic.show_feedback === 1 || (this.topic.show_feedback === 2 && !this.firstTry);
     }
-  }
+
+    private goToNextPage(): void {
+        this.displayCorrectAnswer = false;
+        if (this.questionIndex + 1 < this.topic.questions.length) {
+            const nextId = this.topic.questions[this.questionIndex + 1].id;
+            this.router.navigate(['../', nextId], {relativeTo: this.route});
+        } else {
+            this.router.navigate(['../../', 'completed'], {relativeTo: this.route});
+            // this.router.navigate(['../../../../'], { relativeTo: this.route });
+        }
+    }
 }
