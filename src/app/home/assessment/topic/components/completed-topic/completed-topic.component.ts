6adbf9cace90e4de0a76c48069b3b45934d001f2
adrianmujica <adrian@reliefapplications.org>
2021-06-22 12:49:25 +0100

    feat: not allow go back when we complete an assessment

--- src/app/home/assessment/topic/components/completed-topic/completed-topic.component.ts
+++ src/app/home/assessment/topic/components/completed-topic/completed-topic.component.ts
@@ -1,19 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { AnswerService } from 'src/app/core/services/answer.service';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-completed-topic',
-  templateUrl: './completed-topic.component.html',
-  styleUrls: ['./completed-topic.component.scss']
+    selector: 'app-completed-topic',
+    templateUrl: './completed-topic.component.html',
+    styleUrls: ['./completed-topic.component.scss']
 })
 export class CompletedTopicComponent implements OnInit {
 
-  constructor(
-    private answerService: AnswerService
-  ) { }
+    blockNavigation = true;
 
-  ngOnInit(): void {
-    this.answerService.endTopicAnswer().subscribe();
-  }
+    constructor(
+        private answerService: AnswerService,
+        private router: Router
+    ) {
 
+    }
+
+    ngOnInit(): void {
+        this.answerService.endTopicAnswer().subscribe();
+    }
+
+    /* Shows modal confirmation before leave the page if is evluated topic
+     */
+    canDeactivate(event: any): Observable<boolean> | boolean {
+        if (this.blockNavigation) {
+            return false;
+        }
+        this.blockNavigation = true;
+        return true;
+    }
+
+    goToTopicPage(): void {
+        this.blockNavigation = false;
+        this.router.navigate(['../../../']);
+    }
 }
