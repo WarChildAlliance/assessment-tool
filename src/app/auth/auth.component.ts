2d4d6ae531af16d924055efb78b2759ec4fd285f
Theresa Tobollik <theresa.oort@gmail.com>
2021-10-13 13:00:36 +0200

    adding icon for completed

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../core/services/auth.service';
 import { TranslateService } from '@ngx-translate/core';
+import { CacheService } from '../core/services/cache.service';
+import { GenericConfirmationDialogComponent } from '../shared/components/generic-confirmation-dialog/generic-confirmation-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+
 
 
 @Component({
@@ -17,11 +21,25 @@ export class AuthComponent implements OnInit {
 
     constructor(
         private authService: AuthService,
-        public translate: TranslateService
+        public translate: TranslateService,
+        private cacheService: CacheService,
+        public dialog: MatDialog,
     ) {
     }
 
     ngOnInit(): void {
+        if (!this.cacheService.networkStatus.getValue()){
+            const dialogRef = this.dialog.open(GenericConfirmationDialogComponent, {
+                disableClose: true,
+                data: {
+                  title: 'Login not possible',
+                  content: 'You need to have an internet connection in order to login.',
+                  cancelBtn: false,
+                  confirmBtnText: 'Close',
+                  confirmBtnColor: 'warn',
+                }
+              });
+        }
     }
 
     onSubmit(): void {
