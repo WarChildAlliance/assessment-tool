fc47ccc0c2ad5e5a4af8b11684ec72b0a0cce896
Theresa Tobollik <theresa.oort@gmail.com>
2021-10-29 11:05:30 +0200

    linting

---
+++ src/app/constants/tutorial.dictionary.ts
@@ -0,0 +1,55 @@
+export const TutorialContent =
+    {
+        completed: true,
+        pages:
+        [
+            {
+            pageID: 'assessments-page',
+            completed: true,
+            steps: [
+                {
+                text: 'Welcome',
+                pictureURL: '',
+                audioURL: 'assessmentsAssisstant.ogg',
+                },
+                {
+                text: 'to you first assessment',
+                pictureURL: '',
+                audioURL: 'assessmentsAssisstant.ogg',
+                },
+            ]
+            },
+            {
+            pageID: 'topics-page',
+            completed: true,
+            steps: [
+                {
+                text: 'these are topics',
+                pictureURL: '',
+                audioURL: 'assessmentsAssisstant.ogg',
+                },
+                {
+                text: 'cool right',
+                pictureURL: '',
+                audioURL: 'assessmentsAssisstant.ogg',
+                },
+            ]
+            },
+            {
+            pageID: 'assessments-page',
+            completed: true,
+            steps: [
+                {
+                text: 'you are back',
+                pictureURL: '',
+                audioURL: 'assessmentsAssisstant.ogg',
+                },
+                {
+                text: 'why?',
+                pictureURL: '',
+                audioURL: 'assessmentsAssisstant.ogg',
+                },
+            ]
+            },
+        ]
+    };
