0b31fb7a263ae05c29374804b1422f9e724312f3
Theresa Tobollik <theresa.oort@gmail.com>
2021-11-01 13:58:25 +0100

    add audio to tutorial content

--- src/app/constants/tutorial.dictionary.ts
+++ src/app/constants/tutorial.dictionary.ts
@@ -15,44 +15,43 @@ export const TutorialContent =
                 {
                 text: '',
                 pictureURL: 'T2-homescreen.png',
-                audioURL: 'slideshow_2.ogg',
-                },
-                {
-                text: '',
-                pictureURL: 'T3-helper-bee.svg',
                 audioURL: 'slideshow_3.ogg',
                 },
                 {
                 text: '',
-                pictureURL: 'T4-profile.png',
+                pictureURL: 'T3-helper-bee.svg',
                 audioURL: 'slideshow_4.ogg',
                 },
                 {
                 text: '',
-                pictureURL: 'T5-honeypot.png',
+                pictureURL: 'T4-profile.png',
                 audioURL: 'slideshow_5.ogg',
                 },
                 {
                 text: '',
-                pictureURL: 'T6-star.png',
+                pictureURL: 'T5-honeypot.png',
                 audioURL: 'slideshow_6.ogg',
                 },
                 {
                 text: '',
-                pictureURL: 'T7-exit.png',
+                pictureURL: 'T6-star.png',
                 audioURL: 'slideshow_7.ogg',
                 },
                 {
                 text: '',
-                pictureURL: 'T8-tutorial.png',
+                pictureURL: 'T7-exit.png',
                 audioURL: 'slideshow_8.ogg',
                 },
                 {
                 text: '',
-                pictureURL: 'T9-tutorial-click.png',
+                pictureURL: 'T8-tutorial.png',
                 audioURL: 'slideshow_9.ogg',
                 },
-
+                {
+                text: '',
+                pictureURL: 'T9-tutorial-click.png',
+                audioURL: 'slideshow_10.ogg',
+                },
             ]
             },
             {
@@ -60,14 +59,184 @@ export const TutorialContent =
             completed: true,
             steps: [
                 {
-                text: 'these are topics',
+                text: '',
                 pictureURL: '',
-                audioURL: 'assessmentsAssisstant.ogg',
+                audioURL: 'slideshow_11.ogg',
                 },
                 {
-                text: 'cool right',
+                text: '',
                 pictureURL: '',
-                audioURL: 'assessmentsAssisstant.ogg',
+                audioURL: 'slideshow_12.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_13.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_14.ogg',
+                },
+            ]
+            },
+            {
+            pageID: 'question-number-line-page',
+            completed: true,
+            steps: [
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_15.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_16.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_17.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_18.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_19.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_20.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_21.ogg',
+                },
+            ]
+            },
+            {
+            pageID: 'question-select-page',
+            completed: true,
+            steps: [
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_23.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_24.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_25.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_26.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_27.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_28.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_29.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_30.ogg',
+                },
+            ]
+            },
+            {
+            pageID: 'topics-page',
+            completed: true,
+            steps: [
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_31.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_32.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_33.ogg',
+                }
+            ]
+            },
+            {
+            pageID: 'question-select-page',
+            completed: true,
+            steps: [
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_34.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_35.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_36.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_37.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_38.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_39.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_40.ogg',
+                }
+            ]
+            },
+            {
+            pageID: 'completed-topic-page',
+            completed: true,
+            steps: [
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_41.ogg',
                 },
             ]
             },
@@ -76,14 +245,46 @@ export const TutorialContent =
             completed: true,
             steps: [
                 {
-                text: 'you are back',
+                text: '',
                 pictureURL: '',
-                audioURL: 'assessmentsAssisstant.ogg',
+                audioURL: 'slideshow_42.ogg',
+                },
+            ]
+            },
+            {
+            pageID: 'profile-page',
+            completed: true,
+            steps: [
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_43.ogg',
                 },
                 {
                 text: 'why?',
                 pictureURL: '',
-                audioURL: 'assessmentsAssisstant.ogg',
+                audioURL: 'slideshow_44.ogg',
+                },
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_45.ogg',
+                },
+                {
+                text: 'why?',
+                pictureURL: '',
+                audioURL: 'slideshow_46.ogg',
+                },
+            ]
+            },
+            {
+            pageID: 'assessments-page',
+            completed: true,
+            steps: [
+                {
+                text: '',
+                pictureURL: '',
+                audioURL: 'slideshow_47.ogg',
                 },
             ]
             },
