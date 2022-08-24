286375bec16f1b4b5f85fdf17eb843621d43eef8
Nicolas Bernes <nicolas.bernes@epitech.eu>
2022-08-24 14:04:58 +0000

    GOBEE-288: Add sound on in/correct answers

---
+++ src/app/home/assessment/topic/components/audio-feedback/audio-feedback.dictionary.ts
@@ -0,0 +1,10 @@
+export const FeedbackAudio = {
+  rightAnswer: [
+    '/assets/audios/answers/Cartoon Poof.wav',
+    '/assets/audios/answers/Children Voice - Great 2.wav'
+  ],
+  wrongAnswer: [
+    '/assets/audios/answers/Cartoon Failure.wav',
+    '/assets/audios/answers/Children Voice - Ouch.wav'
+  ]
+};
