5e3b3b7f48df1682477554047b548ae3ca44d56b
gustavo-candido <gustavocandidofreitas@gmail.com>
2022-10-13 09:19:10 -0300

    insert audio

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,7 +1,15 @@
-import { Component, OnChanges, OnDestroy, OnInit, SimpleChanges } from "@angular/core";
+import {
+  Component,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from "@angular/core";
 import { Router } from "@angular/router";
+import { TranslateService } from "@ngx-translate/core";
 import { Subscription } from "rxjs";
 import { User } from "../core/models/user.model";
+import { TextToSpeechService } from "../core/services/text-to-speech.service";
 import { UserService } from "../core/services/user.service";
 
 @Component({
@@ -15,44 +23,89 @@ export class IntroComponent implements OnInit, OnDestroy {
   public onBeeLeave = false;
   public userData: User;
   private userSubscription: Subscription = new Subscription();
-  
-  constructor(private userService: UserService, private router: Router) {
-  }
+  private quotes: Record<string, string>;
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private ttsService: TextToSpeechService,
+    public translate: TranslateService
+  ) {}
 
   ngOnInit(): void {
-    this.userSubscription = this.userService.getUser().subscribe(async (data) => {
-      this.userData = data;
-      await this.runEnterAnimation();
-      await this.runSecondSpeech();
-      await this.runBeeLeaves(); 
-      this.userService.updateUserNoCache({ id: this.userData.id,see_intro: false}).subscribe(() => {
-        this.router.navigate(['/']);
+    this.userSubscription = this.userService
+      .getUser()
+      .subscribe(async (userData) => {
+        this.userData = userData;
       });
-    });
-    
   }
-  
+
+  async init(): Promise<void> {
+    await this.setQuotes(this.userData);
+
+    await this.runEnterAnimation();
+    await this.runSecondSpeech();
+    await this.runBeeLeaves();
+
+    this.userService
+      .updateUserNoCache({ id: this.userData.id, see_intro: false })
+      .subscribe(() => {
+        this.router.navigate(["/"]);
+      });
+  }
+
   ngOnDestroy(): void {
     this.userSubscription.unsubscribe();
   }
 
-  timeout(ms:number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  timeout(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
-  
+
   async runEnterAnimation() {
     this.onEnterAnimation = true;
+    await this.playAudio(this.quotes["general.hi"]);
+    await this.timeout(500);
+    await this.playAudio(this.quotes["intro.enterBallon"]);
     await this.timeout(4500);
     this.onEnterAnimation = false;
   }
-  
+
   async runSecondSpeech() {
     this.onSecondBallon = true;
+    await this.playAudio(this.quotes["intro.secondBallon"]);
     await this.timeout(4500);
     this.onSecondBallon = false;
   }
 
   async runBeeLeaves() {
     this.onBeeLeave = true;
+  }
+
+  async setQuotes(userData: User) {
+    this.quotes = await this.translate
+      .get(["general.hi", "intro.enterBallon", "intro.secondBallon"])
+      .toPromise();
+
+    this.quotes = {
+      ...this.quotes,
+      "general.hi": this.quotes["general.hi"].replace(
+        "{{name}}",
+        userData.first_name
+      ),
+    };
+  }
+
+  async playAudio(quote: string) {
+    const audioURL = await this.ttsService
+      .getSynthesizedSpeech(
+        this.userData.language.code === "ENG" ? "en-US" : "ar-XA",
+        quote
+      )
+      .toPromise();
+
+    const titleAudio = new Audio(audioURL);
+    titleAudio.load();
+    await titleAudio.play();
   }
 }
