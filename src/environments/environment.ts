995ae1a8e93a5878cf14e8bf776e91384ebe4869
Hugo <hugo@reliefapplications.org>
2022-11-02 16:19:40 +0100

    remove API key for TSS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   API_URL: 'http://localhost:8002',
   TTS_API_URL: 'https://texttospeech.googleapis.com/v1/text:synthesize',
-  TTS_API_KEY: 'AIzaSyAmR4bv7FuWavYEqS2DqTUifnlFp_Ay1GU'
+  // TTS_API_KEY: 'AIzaSyAmR4bv7FuWavYEqS2DqTUifnlFp_Ay1GU'
 };
 
 /*
