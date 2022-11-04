9a07cc38002b3da73a5982d4049e0bc0bcb621fa
Pacôme Rivier <pacome@reliefapplications.org>
2022-11-04 15:54:04 +0100

    replace azure api default url with cname

--- src/environments/environment.azure.ts
+++ src/environments/environment.azure.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://assessment-tool-api.westeurope.cloudapp.azure.com',
+  API_URL: 'https://api.gobee-education.org',
   TTS_API_URL: 'https://texttospeech.googleapis.com/v1/text:synthesize',
   TTS_API_KEY: ''
 };
