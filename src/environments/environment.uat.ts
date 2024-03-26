df5bd6cf60bb0afdce1202370a8f46dc007b5756
WarChildAlliance <161589653+WarChildAlliance@users.noreply.github.com>
2024-03-26 10:04:25 +0100

    Update environment.uat.ts

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   API_URL: 'https://glmt-api.development.humanitarian.tech',
   TTS_API_URL: 'https://texttospeech.googleapis.com/v1/text:synthesize',
-  TTS_API_KEY: 'AIzaSyBlSZIqWoQAqNZ6BL7-eJ7ot-rt9x4MRT0'
+  // Add your google Text to Spech API key here for the functionnality to work:
+  TTS_API_KEY: ''
 };
